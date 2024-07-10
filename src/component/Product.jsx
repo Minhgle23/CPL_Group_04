05115d1381eaa6455fccb61c18f5e8ef33694d60
Phan Manh <manhpdhe161197@fpt.edu.vn>
2024-07-10 11:39:14 +0700

    Login

---
+++ src/component/Product.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+
+function Product() {
+  const [listProduct, setListProduct] = useState([]);
+
+  
+  useEffect(() => {
+    fetch("http://localhost:9999/products")
+      .then((res) => res.json())
+      .then((json) => setListProduct(json));
+  }, []);
+  
+  
+  const cardItem = (item) => {
+    return (
+      <div className="card my-5 py-4" key={item.id} style={{ width: "18rem" }}>
+        <img
+          src={item.image}
+          class="card-img-top"
+          alt={item.title}
+          height={"300px"}
+        />
+        <div className="card-body text-center">
+          <h5 className="card-title">{item.title}</h5>
+          <p className="lead">${item.price}</p>
+          <NavLink to={`/products/${item.id}`} className="btn btn-outline-dark">
+            Buy Now
+          </NavLink>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <div className="container py-5">
+        <div className="row">
+          <div className="col-12 text-center">
+            <h1>Product</h1>
+            <hr />
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row justify-content-around">
+          {listProduct.map(cardItem)}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Product;
---
+++ src/component/ProductDetail.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from "react";
+import { useParams } from "react-router";
+import { useState } from "react";
+
+const ProductDetail = () => {
+  const [product, setProduct] = useState({});
+  
+  const {id} = useParams();
+  console.log(id);
+  useEffect(() => {
+    fetch(`http://localhost:9999/products/${id}`)
+      .then((res) => res.json())
+      .then((json) => setProduct(json));
+  }, [id]);
+
+  console.log(product);
+
+
+
+  return (
+    <>
+      <div className="container my-5 py-3">
+        <div className="row">
+          <div className="col-md-6 d-flex justify-content-center mx-auto product">
+            <img src={product.image} alt={product.title} height="400px" />
+          </div>
+          <div className="col-md-6 d-flex flex-column justify-content-center">
+            <h1 className="display-5 fw-bold">{product.title}</h1>
+            <hr />
+            <h2 className="my-4">${product.price}</h2>
+            <p className="lead">{product.description}</p>
+            <button
+              className="btn btn-outline-primary my-5"
+            >
+                Add to cart hehe
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProductDetail;
