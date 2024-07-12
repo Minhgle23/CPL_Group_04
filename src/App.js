b7b7b413589c45920879fb68b5a79c7ed7a69d92
annthe170622 <annthe170622@fpt.edu.vn>
2024-07-12 10:29:38 +0700

    fix conflict

--- src/App.js
+++ src/App.js
@@ -1,51 +1,34 @@
-
 import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 import { Row, Col } from 'react-bootstrap';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './component/Home';
-
-import Navbar from './component/common/Navbar';
 import Product from './component/Product';
 import ProductDetail from './component/ProductDetail';
 import Login from './component/Login';
 import Profile from './component/Profile';
-
-import CustomNavbar from './component/common/Navbar';
 import { CartProvider } from './component/CartContext';
-
-
-
+import CustomNavbar from './component/common/Navbar';
 
 function App() {
   return (
-
     <CartProvider>
-    <BrowserRouter>
-      <CustomNavbar/>
-      <Row>
-        <Col xs={12} sm={12} md={12} >
-    <BrowserRouter>
-      <Navbar/>
-      <Row>
-        <Col xs={12} sm={12} md={12}>
-
-          <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/products' element={<Product/>}/>
-            <Route path='/products/:id' element={<ProductDetail/>}/>
-            <Route path='/login' element={<Login/>}/>
-            <Route path='/profile' element={<Profile/>}/>
-          </Routes>
-        </Col>
-      </Row>
-    
-
-    </BrowserRouter >
-
+      <BrowserRouter>
+        <CustomNavbar />
+        <Row>
+          <Col xs={12} sm={12} md={12}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/products" element={<Product />} />
+              <Route path="/products/:id" element={<ProductDetail />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+            </Routes>
+          </Col>
+        </Row>
+      </BrowserRouter>
     </CartProvider>
-
   );
 }
 
--- src/component/common/Navbar.jsx
+++ src/component/common/Navbar.jsx
@@ -1,16 +1,11 @@
-
 import React, { useState, useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
-import CartContext from '../CartContext';
-
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import { Navbar, Nav, Container, Dropdown, Button } from 'react-bootstrap';
+import CartContext from '../CartContext';
 
 function CustomNavbar() {
   const [user, setUser] = useState(null);
-  const {totalCartItem} = useContext(CartContext);
-
+  const { totalCartItem } = useContext(CartContext);
 
   useEffect(() => {
     const storedUser = localStorage.getItem('user');
@@ -22,18 +17,18 @@ function CustomNavbar() {
   const handleLogout = () => {
     localStorage.removeItem('user');
     setUser(null);
-    window.location.href = '/'; // Chuyển hướng đến trang chính sau khi logout
+    window.location.href = '/'; // Redirect to the main page after logout
   };
 
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#">Ecommer</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">Ecommer</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbar-nav" />
         <Navbar.Collapse id="navbar-nav">
           <Nav className="me-auto">
             <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/products">Product</Nav.Link>
+            <Nav.Link as={Link} to="/products">Products</Nav.Link>
             <Nav.Link href="#">Contact</Nav.Link>
             <Nav.Link href="#">Link</Nav.Link>
           </Nav>
@@ -41,7 +36,7 @@ function CustomNavbar() {
             {user ? (
               <Dropdown align="end">
                 <Dropdown.Toggle variant="outline-dark" id="dropdown-basic">
-                  Hello, {user.name.firstname} 
+                  Hello, {user.name.firstname}
                 </Dropdown.Toggle>
                 <Dropdown.Menu>
                   <Dropdown.Item as={Link} to="/profile">View Profile</Dropdown.Item>
@@ -55,9 +50,8 @@ function CustomNavbar() {
             )}
           </Nav>
           <Nav>
-            <Button variant="outline-dark" as={Link} to ="/cart"
-                <i className='fa fa-shopping-cart'> Cart({totalCartItem}) </i>
-
+            <Button variant="outline-dark" as={Link} to="/cart">
+              <i className="fa fa-shopping-cart"></i> Cart({totalCartItem})
             </Button>
           </Nav>
         </Navbar.Collapse>
@@ -67,4 +61,3 @@ function CustomNavbar() {
 }
 
 export default CustomNavbar;
-
